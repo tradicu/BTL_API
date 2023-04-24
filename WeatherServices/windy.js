64f68c622816436551f5f793be9026d564ba72fa
vkchung <129825204+vkchung@users.noreply.github.com>
2023-04-25 01:37:34 +0700

    tuan

--- WeatherServices/windy.js
+++ WeatherServices/windy.js
@@ -1,9 +1,9 @@
 ﻿const options = {
     key: '7kYbFRfWpWle2AlQoD75rmxz4FBnZlEa', // REPLACE WITH YOUR KEY !!!
-    lat: 50.4,
-    lon: 14.3,
+    lat: 21.0245,
+    lon: 105.8412,
     //const overlays = ['rain', 'wind', 'temp', 'clouds'];
-    overlay: 'rain', //thay đổi màu theo thời tiết: gió, mây,...
+    //overlay: 'rain', //thay đổi màu theo thời tiết: gió, mây,...
     timestamp: "2021-09-01T12:00:00Z", //thời điểm bạn muốn hiển thị thông tin dữ liệu
     level: "surface", //để hiển thị dữ liệu trên mức bề mặt.       
     zoom: 5,
@@ -12,11 +12,11 @@
 windyInit(options, windyAPI => {
     const {
         picker,
-        utils,
-        broadcast,
+        utils, //chuyển dữ liệu thời tiết từ 1 mảng  thành 1 đối tượng
+        broadcast, //lưu trữ và quản lý dữ liệu
         store
     } = windyAPI;
-
+    // windyAPI.broadcast.setView([lat, lon], 10);
     picker.on('pickerOpened', ({
         lat,
         lon,
@@ -25,7 +25,7 @@ windyInit(options, windyAPI => {
     }) => {
         // -> 48.4, 14.3, [ U,V, ], 'wind'
         console.log('opened', lat, lon, values, overlay);
-
+        //sử dụng hàm `wind2obj()` trong đối tượng `utils` của Windy API.
         const windObject = utils.wind2obj(values);
         console.log(windObject);
     });
@@ -33,7 +33,7 @@ windyInit(options, windyAPI => {
     picker.on('pickerMoved', ({
         lat,
         lon,
-        values,
+        values, //Giá trị thông tin thời tiết tại vị trí được chọn, được biểu diễn dưới dạng một mảng chứa các gía trị vector của gió tại đó.
         overlay
     }) => {
         // picker was dragged by user to latLon coords
@@ -43,7 +43,7 @@ windyInit(options, windyAPI => {
     picker.on('pickerClosed', () => {
         // picker was closed
     });
-
+    // Lưu lại tọa độ thay đổi
     store.on('pickerLocation', ({
         lat,
         lon
@@ -57,12 +57,12 @@ windyInit(options, windyAPI => {
         console.log('location changed', lat, lon, values, overlay);
     });
 
-    // Wait since wather is rendered
+    // Tạo và hiển thị picker
     broadcast.once('redrawFinished', () => {
         // tọa độ cắm cọc
         picker.open({
-            lat: 50.4,
-            lon: 14.3
+            lat: 21.0245,
+            lon: 105.8412
         });
     });
 });
